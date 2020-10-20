682351a576a88566f0643b9c93f7e8981075447e
Jonas A <jonas.askling@student.forsbergsskola.se>
2020-10-20 15:07:04 +0200

    Working on Gold Press
    Gold press can be bought with button as long as the gold is 100 or over.

--- Assets/Scripts/Gold.cs
+++ Assets/Scripts/Gold.cs
@@ -4,30 +4,60 @@ using UnityEngine.UI;
 public class Gold : MonoBehaviour
 {
     public int goldAmount;
+    public int goldAmountPerClick = 5;
     public Text goldAmountText;
 
+    public int goldGenerator;
+    public Text goldGeneratorText;
+    public int generateGoldPerSecond = 1;
+
+    public int GoldAmount
+    {
+        get => PlayerPrefs.GetInt("Gold", 0);
+        set
+        {
+            PlayerPrefs.SetInt("Gold", value);
+            UpdateGoldAmountLabel();
+        }
+    }
+
+    void UpdateGoldAmountLabel()
+    {
+        this.goldAmountText.text = this.GoldAmount.ToString("Gold: 0");
+    }
 
     private void Start()
     {
-        this.goldAmount = PlayerPrefs.GetInt("Gold", 0);
-    }
-
-    private void OnDestroy()
-    {
-        PlayerPrefs.SetInt("Gold", this.goldAmount);
+        UpdateGoldAmountLabel();
     }
 
     void Update()
     {
-        this.goldAmountText.text = this.goldAmount.ToString("Gold: 0");
         if (Input.GetMouseButtonDown(0))
         {
             ProduceGold();
         }
+        this.goldGeneratorText.text = this.goldGenerator.ToString("Gold Generators: 0");
     }
 
     public void ProduceGold()
     {
-        this.goldAmount += 5;
+        this.GoldAmount += this.goldAmountPerClick;
     }
+
+    public void GoldGenerator()
+    {
+        if (GoldAmount > 100)
+        {
+            this.goldGenerator += 1;
+            this.GoldAmount -= 100;
+        }
+        else
+        {
+            this.goldGenerator += 0;
+        }
+    }
+
+    
+
 }
