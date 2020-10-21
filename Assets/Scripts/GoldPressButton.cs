d99a4d9c1bfd23ce1ca6cdd44559524f851f2edc
Jonas A <jonas.askling@student.forsbergsskola.se>
2020-10-21 14:30:59 +0200

    Color Changes on Button
    When gold is 100 or over the text is shown in green. And if not, the text is shown in red.

---
+++ Assets/Scripts/GoldPressButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GoldPressButton : MonoBehaviour
+{
+    Color changeColor;
+    Graphic graphic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        graphic = GetComponent<Graphic>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var gold = FindObjectOfType<Gold>();
+        if (gold.GoldAmount <= 100)
+        {
+            changeColor = Color.red;
+            graphic.color = changeColor;
+        }
+        else
+        {
+            changeColor = Color.green;
+            graphic.color = changeColor;
+        }
+    }
+}
